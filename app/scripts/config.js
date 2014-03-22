1a5a729018af44a073467fe680592befd3e8008c
York Chen <qgmzwrym@gmail.com>
2014-03-22 18:03:31 +0800

    work on upload page.

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,68 +1,69 @@
 (function () {
-    'use strict'
+   'use strict';
 
-    var app = angular.module('itmsApp');
+   var app = angular.module('itmsApp');
 
-    var config = {
-        version: '0.0.1',
-        mode: 'development', //development or production
-        baseUrl: 'http://211.144.85.15:8080/ordermanagement/rest/'
-    };
+   var config = {
+      version: '0.0.1',
+      mode: 'development', //development or production
+      baseUrl: 'http://211.144.85.15:8080/ordermanagement/rest/',
+      userID: 10000
+   };
 
-    app.config(['$logProvider', function ($logProvider) {
-        if ($logProvider.debugEnabled) {
-            $logProvider.debugEnabled(true);
-        }
-    }]);
+   app.config(['$logProvider', function ($logProvider) {
+      if ($logProvider.debugEnabled) {
+         $logProvider.debugEnabled(true);
+      }
+   }]);
 
-    app.config(['$provide', function ($provide) {
-        $provide.decorator('$exceptionHandler', function ($log) {
-            return function (exception, cause) {
-                var error = { exception: exception, cause: cause};
-                $log.debug(error.exception);
-                throw error;
+   app.config(['$provide', function ($provide) {
+      $provide.decorator('$exceptionHandler', function ($log) {
+         return function (exception, cause) {
+            var error = { exception: exception, cause: cause };
+            $log.debug(error.exception);
+            throw error;
+         };
+      });
+
+      $provide.decorator('$q', function ($delegate) {
+         var defer = $delegate.defer;
+         $delegate.defer = function () {
+            var deferred = defer();
+            deferred.promise.success = function (fn) {
+               deferred.promise.then(function (value) {
+                  fn(value);
+               });
+               return deferred.promise;
             };
-        });
 
-        $provide.decorator('$q', function ($delegate) {
-            var defer = $delegate.defer;
-            $delegate.defer = function () {
-                var deferred = defer();
-                deferred.promise.success = function (fn) {
-                    deferred.promise.then(function (value) {
-                        fn(value);
-                    });
-                    return deferred.promise;
-                };
-
-                deferred.promise.error = function (fn) {
-                    deferred.promise.then(null, function (value) {
-                        fn(value);
-                    });
-                    return deferred.promise;
-                };
-                return deferred;
+            deferred.promise.error = function (fn) {
+               deferred.promise.then(null, function (value) {
+                  fn(value);
+               });
+               return deferred.promise;
             };
-            return $delegate;
-        });
+            return deferred;
+         };
+         return $delegate;
+      });
 
-        $provide.decorator('$http', function ($delegate) {
-            $delegate['postXSRF'] = function(url,data) {
-                return $delegate({
-                    method: 'POST',
-                    url: url,
-                    data: $.param(data),
-                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-                });
-            };
-            return $delegate;
-        });
-    }]);
+      $provide.decorator('$http', function ($delegate) {
+         $delegate['postXSRF'] = function (url, data) {
+            return $delegate({
+               method: 'POST',
+               url: url,
+               data: $.param(data),
+               headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            });
+         };
+         return $delegate;
+      });
+   }]);
 
-//    app.config(function (cfpLoadingBarProvider) {
-//        cfpLoadingBarProvider.includeSpinner = false;
-//    });
+   //    app.config(function (cfpLoadingBarProvider) {
+   //        cfpLoadingBarProvider.includeSpinner = false;
+   //    });
 
-    app.value('config', config);
+   app.value('config', config);
 
 }());
--- app/scripts/controllers/RequirementUploadCtrl.js
+++ app/scripts/controllers/RequirementUploadCtrl.js
@@ -1,24 +1,14 @@
 ;
-(function () {
+(function (angular) {
    "use strict";
    angular.module("itmsApp").controller("RequirementUploadCtrl",
-       ["$scope", "config", "$compile", "$modal", "$log", "common", requirementUploadCtrl]);
+       ["$scope", "$http", "config", "$compile", "$modal", "$log", "common", controller]);
 
-   function requirementUploadCtrl($scope, config, $compile, $modal, $log, common) {
+   function controller($scope, $http, config, $compile, $modal, $log, common) {
       $scope.module = "需求管理";
       $scope.title = "站点上载";
-
       $scope.selectFile = function (element) {
          $("#fileDisplay").val($(element).val());
-      };
-      $scope.showMessage = function (msg) {
-         $.smallBox({
-            title: "操作回执",
-            content: "<i class='fa fa-clock-o'></i>" + msg,
-            color: "#659265",
-            iconSmall: "fa fa-check fa-2x fadeInRight animated",
-            timeout: 4000
-         });
       };
       $scope.getExtName = function (name) {
          var index = name.lastIndexOf(".");
@@ -27,81 +17,98 @@
          var extName = name.substring(index + 1).toLowerCase();
          return extName;
       };
-
+      $scope.columns = [
+           /* { "mData": "", "sTitle": "detail" },*/
+            { "mData": "erType", "sTitle": "类型" },
+            { "mData": "id.customerOrder1", "sTitle": "客户订单号" },
+            { "mData": "id.erITN", "sTitle": "ERITN" },
+            { "mData": "matIID", "sTitle": "物料描述" },
+            { "mData": "amt", "sTitle": "数量" },
+            { "mData": "packNum", "sTitle": "箱号" },
+            { "mData": "depAreaCode", "sTitle": "客户名称" },
+            { "mData": "customerOrder2", "sTitle": "客户运单号" },
+            { "mData": "customerOrder3", "sTitle": "客户出库号" },
+            { "mData": "depCustomer", "sTitle": "收货地" },
+            { "mData": "recLocCode", "sTitle": "目的地" },
+            { "mData": "reqDelDate", "sTitle": "要求到达日期" }
+      ];
       $scope.updateTable = function (data) {
          $scope.previewData = data;
-         $("#datatable_col_reorder_wrapper").remove();
+         $scope.test = $scope.previewData.list;
+         $scope.$apply();
 
-         var table =
-            '<table id="datatable_col_reorder" class="table table-striped table-hover">' +
-               '<thead>' +
-               '<tr>' +
-               '<th><i class="fa fa-search"></i></th>' +
-               '<th>类型</th>' +
-               '<th>客户订单号</th>' +
-               '<th>ERITN</th>' +
-               '<th>物料描述</th>' +
-               '<th>数量</th>' +
-               '<th>箱号</th>' +
-               '<th>客户名称</th>' +
-               '<th>客户运单号</th>' +
-               '<th>客户出库号</th>' +
-               '<th>收货地</th>' +
-               '<th>目的地</th>' +
-               '<th>要求到达日期</th>' +
-               '</tr>' +
-               '</thead>' +
-               '<tbody>' +
-               '<tr ng-repeat="item in previewData.list">' +
-               '<td><a href="javascript:void(0);" data-target="#ERTemp" data-toggle="modal"><i class="fa fa-search"></i></a></td>' +
-               '<td>{{item.erType}}</td>' +
-               '<td>{{item.id.customerOrder1}}</td>' +
-               '<td>{{item.id.erITN}}</td>' +
-               '<td>{{item.matIID}}</td>' +
-               '<td>{{item.amt}}</td>' +
-               '<td>{{item.packNum}}</td>' +
-               '<td>{{item.depAreaCode}}</td>' +
-               '<td>{{item.customerOrder2}}</td>' +
-               '<td>{{item.customerOrder3}}</td>' +
-               '<td>{{item.depCustomer}}</td>' +
-               '<td>{{item.recLocCode}}</td>' +
-               '<td>{{item.reqDelDate}}</td>' +
-               '</tr>' +
-               '</tbody>' +
-               '</table>';
+         //$("#datatable_col_reorder_wrapper").remove();
 
-         $compile(table)($scope, function (clonedElement) {
-            $("#tableWidget").append(clonedElement);
-         });
-         try {
-            $scope.$apply();
-         } catch (e) {
-         }
-         $('#datatable_col_reorder').dataTable({
-            "sPaginationType": "bootstrap",
-            "sDom": "R<'dt-top-row'Clf>r<'dt-wrapper't><'dt-row dt-bottom-row'<'row'<'col-sm-6'i><'col-sm-6 text-right'p>>",
-            "fnInitComplete": function (oSettings, json) {
-               $('.ColVis_Button').addClass('btn btn-default btn-sm').html('Columns <i class="icon-arrow-down"></i>');
-            }
-         });
+         //var table =
+         //   '<table id="datatable_col_reorder" class="table table-striped table-hover">' +
+         //      '<thead>' +
+         //      '<tr>' +
+         //      '<th><i class="fa fa-search"></i></th>' +
+         //      '<th>类型</th>' +
+         //      '<th>客户订单号</th>' +
+         //      '<th>ERITN</th>' +
+         //      '<th>物料描述</th>' +
+         //      '<th>数量</th>' +
+         //      '<th>箱号</th>' +
+         //      '<th>客户名称</th>' +
+         //      '<th>客户运单号</th>' +
+         //      '<th>客户出库号</th>' +
+         //      '<th>收货地</th>' +
+         //      '<th>目的地</th>' +
+         //      '<th>要求到达日期</th>' +
+         //      '</tr>' +
+         //      '</thead>' +
+         //      '<tbody>' +
+         //      '<tr ng-repeat="item in previewData.list">' +
+         //      '<td><a href="javascript:void(0);" data-target="#ERTemp" data-toggle="modal"><i class="fa fa-search"></i></a></td>' +
+         //      '<td>{{item.erType}}</td>' +
+         //      '<td>{{item.id.customerOrder1}}</td>' +
+         //      '<td>{{item.id.erITN}}</td>' +
+         //      '<td>{{item.matIID}}</td>' +
+         //      '<td>{{item.amt}}</td>' +
+         //      '<td>{{item.packNum}}</td>' +
+         //      '<td>{{item.depAreaCode}}</td>' +
+         //      '<td>{{item.customerOrder2}}</td>' +
+         //      '<td>{{item.customerOrder3}}</td>' +
+         //      '<td>{{item.depCustomer}}</td>' +
+         //      '<td>{{item.recLocCode}}</td>' +
+         //      '<td>{{item.reqDelDate}}</td>' +
+         //      '</tr>' +
+         //      '</tbody>' +
+         //      '</table>';
+
+         //$compile(table)($scope, function (clonedElement) {
+         //   $("#tableWidget").append(clonedElement);
+         //});
+         //try {
+         //   $scope.$apply();
+         //} catch (e) {
+         //}
+         //$('#datatable_col_reorder').dataTable({
+         //   "sPaginationType": "bootstrap",
+         //   "sDom": "R<'dt-top-row'Clf>r<'dt-wrapper't><'dt-row dt-bottom-row'<'row'<'col-sm-6'i><'col-sm-6 text-right'p>>",
+         //   "fnInitComplete": function (oSettings, json) {
+         //      $('.ColVis_Button').addClass('btn btn-default btn-sm').html('Columns <i class="icon-arrow-down"></i>');
+         //   }
+         //});
       };
       $scope.validate = function () {
          var ele = $("#fileUpload")[0];
          if (!ele.files
              || !ele.files[0])
-            return "请选择上传文件。";
+            return "请选择上传文件";
          var file = ele.files[0];
 
          if (file.size > 2000000)
-            return "文件太大。";
+            return "文件太大";
          var extName = $scope.getExtName(file.name);
          if (extName != "xls" && extName != "xlsx")
-            return "文件类型错误。";
+            return "文件类型错误";
+         return null;
       };
       $scope.handleUpload = function () {
          var msg = $scope.validate();
          if (msg) {
-
             common.messageBox({
                title: "错误信息:",
                content: msg,
@@ -116,27 +123,18 @@
                dataType: "",
                url: config.baseUrl + "ER/Upload",
                success: function (data) {
-                  alert(data);
-                  var re1 = />-</gi;
-                  data = data.replace(re1, '');
-
-                  var re2 = /<[^>]+>/gi;
-                  data = data.replace(re2, '');
-
-                  var result = JSON.parse(data);
-                  $scope.updateTable(result);
-                  if (result && result.key) {
-                     $scope.showMessage("上载内容已成功");
+                  //var re1 = />-</gi;
+                  //data = data.replace(re1, '');
+                  //var re2 = /<[^>]+>/gi;
+                  //data = data.replace(re2, '');
+                  $scope.updateTable(data);
+                  if (data && data.key) {
+                     common.notifier.success("上载内容已成功");
                      $scope.hasUploaded = true;
                      $scope.expandTable();
                   } else {
-
                      $scope.hasUploaded = false;
                   }
-
-               },
-               error: function () {
-                  alert("error");
                }
             };
             $('#checkout-form').ajaxSubmit(submitData);
@@ -146,56 +144,43 @@
          "key": null,
          "list": []
       };
-      $scope.showData = function () {
-         alert(JSON.stringify($scope.previewData));
-      };
-
+      $scope.test = [];
       $scope.confirm = function () {
          if (!$scope.hasUploaded)
-            return false;
+            return;
          var data =
          {
-            "userID": $("#userID").val(),
+            "userID": config.userID,
             "KEY": $scope.previewData.key,
          };
-         $.ajax({
-            type: "POST",
-            url: route.confirm,
-            data: data,
-            success: function (result) {
-               if (result.errorMessage)
-                  $scope.showMessage("上载操作已成功完成");
-               $scope.hasUploaded = false;
-
-            },
-            dataType: "json"
+         $http.post(config.baseUrl + "ER/UploadConfirm" + "?" + $.param(data))
+         //$.post(config.baseUrl + "ER/UploadConfirm", data)
+         .success(function (result) {
+            if (result.errorMessage)
+               common.notifier.success("上载操作已成功完成");
+            $scope.hasUploaded = false;
          });
       };
       $scope.cancel = function () {
          if (!$scope.hasUploaded)
-            return false;
+            return;
          var data =
          {
-            "userID": $("#userID").val(),
+            "userID": config.userID,
             "KEY": $scope.previewData.key,
          };
-         $.ajax({
-            type: "POST",
-            url: route.cancel,
-            data: data,
-            success: function (result) {
+         $http.post(config.baseUrl + "ER/UploadCancel" + "?" + $.param(data))
+         //$.post(config.baseUrl + "ER/UploadCancel", data)
+            .success(function (result) {
                if (result.errorMessage && result.errorMessage == "OK")
-                  $scope.showMessage("上载内容已取消");
+                  common.notifier.success("上载内容已取消");
                $scope.updateTable({
                   "key": null,
                   "list": []
                });
                $scope.hasUploaded = false;
-
-            },
-            dataType: "json"
-         });
-
+            }
+         );
       };
       $scope.hasUploaded = false;
       $scope.expandTable = function () {
@@ -204,7 +189,7 @@
             icon.find(".jarviswidget-toggle-btn").click();
       };
    }
-})();
+})(angular);
 
 
 
