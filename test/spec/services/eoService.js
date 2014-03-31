55a63260bd42ba119e97e62be405e7173f49b936
sean <xujihui1985@outlook.com>
2014-04-01 00:35:51 +0800

    implement unittest for $digest

--- test/spec/services/eoService.js
+++ test/spec/services/eoService.js
@@ -17,4 +17,28 @@ describe('Service: eoService', function() {
     it('should able to create new scope from rootScope', function() {
         expect(scope).not.toBe(null);
     });
+
+    it('count should changed when name changes', function() {
+        scope.name = 'sean';
+        scope.count = 0;
+
+        expect(scope.count).toBe(0);
+
+        scope.$watch('name', function(newValue, oldValue) {
+            console.log(newValue);
+            console.log(oldValue);
+            console.log(arguments);
+            scope.count = scope.count + 1;
+        });
+        expect(scope.count).toBe(0);
+
+        //the count will not change if digest cicle didn't fired
+        scope.name = 'anna';
+        expect(scope.count).toBe(0);
+
+        //the watch expression will only be called once after digest
+        scope.$digest();
+        expect(scope.count).toBe(1);
+
+    });
 });
