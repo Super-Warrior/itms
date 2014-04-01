ab9fdcba7bbc1e032a294e4a293fa01a4abfd29f
sean <xujihui1985@outlook.com>
2014-04-01 22:42:37 +0800

    abstract gridModal to encapsulate table directive

---
+++ src/common/table/grid.js
@@ -0,0 +1,19 @@
+var swGrid = function(options) {
+    var defaultOptions = {
+        showSelectionCheckbox: false,
+        showDetailView: false,
+        columnDef = [],
+        this.data = []
+    };
+    this.config = $.extend({}, defaultOptions, options);
+}
+
+swGrid.prototype = {
+    constructor: swGrid,
+
+    init: function() {
+
+
+    };
+
+}
