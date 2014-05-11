008e31698644bc1a52ee4865cc33ed3db97a9882
sean <xujihui1985@gmail.com>
2014-05-11 21:03:50 +0800

    add timeline for requirement assignment

--- src/app/common/details/timeline.ctrl.js
+++ src/app/common/details/timeline.ctrl.js
@@ -8,10 +8,16 @@ function TimeLineCtrl($scope, timelineService, data) {
 
     function activate() {
         var queryOption = {},
+            eoNumber;
+        if (data.eoNumber) {
             eoNumber = data.eoNumber.split('/');
-        queryOption['eo'] = eoNumber[0];
-        queryOption['er'] = eoNumber[1];
-        queryOption['eritn'] = eoNumber[2];
+            queryOption['eo'] = eoNumber[0];
+            queryOption['er'] = eoNumber[1];
+            queryOption['eritn'] = eoNumber[2];
+        } else if (data.requirementDetail) {
+            queryOption['er'] = data.requirementDetail.pk.erID;
+            queryOption['eritn'] = data.requirementDetail.pk.erITN;
+        }
         timelineService.getEventTimeLine(queryOption).then(function (result) {
             if (result.data.errorMessage) {
                 $scope.timelines = [];
