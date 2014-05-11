5eda6c88922bd0684d45f76db2bde4e9cd6fba7a
chunchill <chunchill@163.com>
2014-05-11 21:03:41 +0800

    working on search of EOMaintain module.

--- src/app/transport/eoMaintain/eoMaintainService.js
+++ src/app/transport/eoMaintain/eoMaintainService.js
@@ -1,12 +1,31 @@
 angular.module('itms.transport.eoMaintain')
 .factory('eoMaintainService', ['$http', 'config', "configService", function ($http, config, configService){
     	// body...
-   
-        var searchUrl= config.baseUrl + 'EO/EOQuickSearch';
+    	
+    	var searchUrl= config.baseUrl + 'EO/EOQuickSearch';
 
-        return function (param) {
-           var data = param;
-           return $http.postXSRF(searchUrl, data);
-       }
+    	return {
+    		quickSearch:quickSearch,
+            getResultPartial:getResultPartial
+    	}
 
-   }]);
+    	function quickSearch(param) {
+    		var data = param;
+    		return $http.postXSRF(searchUrl, data);
+    	}
+
+         function getResultPartial(items) {
+        return items.map(mapResult);
+
+        function mapResult(item) {
+            return {
+                eo: item.dn.eo, 
+                erID: item.erHead.erID, 
+                erITN: item.erItem.pk.erITN, 
+                eoStatus: item.dn.eoStatus
+            };
+        }
+    }
+
+
+    }]);
