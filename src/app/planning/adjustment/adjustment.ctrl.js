b6a9ef16da81396ffc9e889acd006fe794b55855
sean <xujihui1985@gmail.com>
2014-05-13 06:39:09 +0800

    fix issue can't searchcustomer in requirement adjustment fix the issue that can do action when on lineitem selected show detail on table at last column

--- src/app/planning/adjustment/adjustment.ctrl.js
+++ src/app/planning/adjustment/adjustment.ctrl.js
@@ -2,162 +2,171 @@ angular.module('itms.planning.adjustment')
     .controller('EOAssignAdjustCtrl', ['$scope', '$modal', '$log', 'orderService', 'common', EOAssignAdjustCtrl]);
 
 function EOAssignAdjustCtrl($scope, $modal, $log, orderService, common) {
-   var notifier = common.notifier;
+    var notifier = common.notifier;
 
-   $scope.module = '计划';
-   $scope.title = '分配调整';
+    $scope.module = '计划';
+    $scope.title = '分配调整';
 
-   $scope.orders = [];
+    $scope.orders = [];
 
-   $scope.selectedItems = [];
-   $scope.selectedSite = [];
-   $scope.selectedPosition = { "dep": [], "rec": [] };
-   $scope.selectedCustomer = { "dep": [], "rec": [] };
+    $scope.selectedItems = [];
+    $scope.selectedSite = [];
+    $scope.selectedPosition = { "dep": [], "rec": [] };
+    $scope.selectedCustomer = { "dep": [], "rec": [] };
 
-   $scope.reset = function () {
-      $scope.selectedCustomer = { "dep": [], "rec": [] };
-      $scope.selectedSite = [];
-      $scope.selectedPosition = { "dep": [], "rec": [] };
-      $scope.createDate = "";
-      $scope.user = "";
-   };
+    $scope.reset = function () {
+        $scope.selectedCustomer = { "dep": [], "rec": [] };
+        $scope.selectedSite = [];
+        $scope.selectedPosition = { "dep": [], "rec": [] };
+        $scope.createDate = "";
+        $scope.user = "";
+    };
 
-   $scope.searchAssignableRequest = function () {
-      orderService.queryAll().success(function (data) {
-         $scope.orders = orderService.getRequirementPartial(data);
-      });
-   };
+    $scope.searchAssignableRequest = function () {
+        orderService.queryAll().success(function (data) {
+            $scope.orders = orderService.getRequirementPartial(data);
+        });
+    };
 
-   $scope.reallocate = function () {
-      common
-          .messageBox({
-             title: "分配至其它运单",
-             content: "请输入运单号",
-             input: "text",
-             placeholder: "EO运单编号"
-          })
-          .success(reallocate)
-          .error(cancel);
+    $scope.reallocate = function reallocate() {
+        if ($scope.selectedItems.length > 0) {
+            common
+                .messageBox({
+                    title: "分配至其它运单",
+                    content: "请输入运单号",
+                    input: "text",
+                    placeholder: "EO运单编号"
+                })
+                .success(reallocate)
+                .error(cancel);
 
-      function reallocate(value) {
-         orderService
-             .erAssignChange({
-                selectedItems: $scope.selectedItems,
-                eoid: value
-             })
-             .success(function () {
-                notifier.success("已成功分配至订单" + value);
-                $scope.searchAssignableRequest();
-             });
-      }
-
-      function cancel() {
-         notifier.cancel("操作取消...");
-      }
-
-   };
-
-   $scope.cancelAssignment = function () {
-      common.messageBox({
-         title: "提示信息!",
-         content: "是否取消所选择记录的运单分配?"
-      })
-          .success(cancelAssignment)
-          .error(function () {
-             notifier.cancel();
-          });
-
-      function cancelAssignment() {
-         orderService
-             .erDeleteAssignment({
-                selectedItems: $scope.selectedItems
-             })
-             .success(function () {
-                notifier.success("已成功取消运单分配...");
-                $scope.searchAssignableRequest();
-             });
-      }
-   };
-
-   $scope.disableAction = function () {
-      return $scope.selectedItems.length === 0;
-   };
-
-   $scope.columns = [
-       { "mData": "eoID", "sTitle": "EO" },
-       { "mData": "erID", "sTitle": "ER" },
-       { "mData": "erITN", "sTitle": "ERITN" },
-       { "mData": "erType", "sTitle": "类型" },
-       { "mData": "erTag", "sTitle": "特殊" },
-       { "mData": "depCustomer", "sTitle": "发货方" },
-       { "mData": "recCustomer", "sTitle": "收货方" },
-       { "mData": "customerOrder1", "sTitle": "客户订单号" },
-       { "mData": "customerOrder2", "sTitle": "客户运单号" },
-       { "mData": "customerOrder3", "sTitle": "客户出库号" },
-       { "mData": "matIID", "sTitle": "物料" },
-       { "mData": "packNum", "sTitle": "箱号" },
-       { "mData": "amt", "sTitle": "件数" },
-       { "mData": "reqDelDate", "sTitle": "送达日期" },
-       { "mData": "ertrType", "sTitle": "方式" },
-       { "mData": "ertrVendor", "sTitle": "第三方" }
-   ];
-
-   $scope.searchCriteria = {
-      site: '',
-      senderCode: 'AB00011',
-      receiverCode: 'AB00012',
-      senderLocation: 'AB00013',
-      receiverLocation: 'AB00014',
-      reset: function () {
-         this.site = '';
-         this.senderCode = '';
-         this.receiverCode = '';
-         this.senderLocation = '';
-         this.receiverLocation = '';
-      }
-   };
-
-
-   $scope.searchSite = function () {
-      var modalInstance = $modal.open({
-         templateUrl: "app/planning/searchSite.tpl.html",
-         controller: searchSiteCtrl
-      });
-      modalInstance.result.then(function (keys) {
-         $scope.selectedSite = keys;
-      }, function () {
-         $log.info('Modal dismissed at: ' + new Date());
-      });
-   };
-
-   $scope.searchCustomer = function (type) {
-      var modalInstance = $modal.open({
-         templateUrl: "app/planning/searchCustomer.tpl.html",
-         controller: searchCustomerCtrl,
-         resolve: { "type": type }
-      });
-      modalInstance.result.then(function (selectedItem) {
-         $scope.selectedCustomer[type] = selectedItem;
-      }, function () {
-         $log.info('Modal dismissed at: ' + new Date());
-      });
-   };
-
-   $scope.searchLocation = function () {
-      var modalInstance = $modal.open({
-         templateUrl: "",
-         controller: searchLocationCtrl,
-         resolve: {
-            items: function () {
-               return $scope.selectedItems();
+            function reallocate(value) {
+                orderService
+                    .erAssignChange({
+                        selectedItems: $scope.selectedItems,
+                        eoid: value
+                    })
+                    .success(function () {
+                        notifier.success("已成功分配至订单" + value);
+                        $scope.searchAssignableRequest();
+                    });
             }
-         }
-      });
-      modalInstance.result.then(function (selectedItem) {
-         $scope.selected = selectedItem;
-      }, function () {
-         $log.info('Modal dismissed at: ' + new Date());
-      });
-   };
+
+            function cancel() {
+                notifier.cancel("操作取消...");
+            }
+        }
+    };
+
+    $scope.cancelAssignment = function cancelAssignment() {
+        if ($scope.selectedItems.length > 0) {
+            common.messageBox({
+                title: "提示信息!",
+                content: "是否取消所选择记录的运单分配?"
+            })
+                .success(cancelAssignment)
+                .error(function () {
+                    notifier.cancel();
+                });
+
+            function cancelAssignment() {
+                orderService
+                    .erDeleteAssignment({
+                        selectedItems: $scope.selectedItems
+                    })
+                    .success(function () {
+                        notifier.success("已成功取消运单分配...");
+                        $scope.searchAssignableRequest();
+                    });
+            }
+        }
+    };
+
+    $scope.disableAction = function () {
+        return $scope.selectedItems.length === 0;
+    };
+
+    $scope.columns = [
+        { "mData": "eoID", "sTitle": "EO" },
+        { "mData": "erID", "sTitle": "ER" },
+        { "mData": "erITN", "sTitle": "ERITN" },
+        { "mData": "erType", "sTitle": "类型" },
+        { "mData": "erTag", "sTitle": "特殊" },
+        { "mData": "depCustomer", "sTitle": "发货方" },
+        { "mData": "recCustomer", "sTitle": "收货方" },
+        { "mData": "customerOrder1", "sTitle": "客户订单号" },
+        { "mData": "customerOrder2", "sTitle": "客户运单号" },
+        { "mData": "customerOrder3", "sTitle": "客户出库号" },
+        { "mData": "matIID", "sTitle": "物料" },
+        { "mData": "packNum", "sTitle": "箱号" },
+        { "mData": "amt", "sTitle": "件数" },
+        { "mData": "reqDelDate", "sTitle": "送达日期" },
+        { "mData": "ertrType", "sTitle": "方式" },
+        { "mData": "ertrVendor", "sTitle": "第三方" }
+    ];
+
+    $scope.searchCriteria = {
+        site: '',
+        senderCode: 'AB00011',
+        receiverCode: 'AB00012',
+        senderLocation: 'AB00013',
+        receiverLocation: 'AB00014',
+        reset: function () {
+            this.site = '';
+            this.senderCode = '';
+            this.receiverCode = '';
+            this.senderLocation = '';
+            this.receiverLocation = '';
+        }
+    };
+
+
+    $scope.searchSite = function () {
+        var modalInstance = $modal.open({
+            templateUrl: "app/planning/searchSite.tpl.html",
+            controller: searchSiteCtrl
+        });
+        modalInstance.result.then(function (keys) {
+            $scope.selectedSite = keys;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    $scope.searchCustomer = function (type) {
+        var modalInstance = $modal.open({
+            templateUrl: "app/planning/searchCustomer.tpl.html",
+            controller: searchCustomerCtrl,
+            resolve: {
+                type: function () {
+                    return type;
+                }
+            }
+        });
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selectedCustomer[type] = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    $scope.searchLocation = function () {
+        var modalInstance = $modal.open({
+            templateUrl: "",
+            controller: searchLocationCtrl,
+            resolve: {
+                items: function () {
+                    return $scope.selectedItems();
+                }
+            }
+        });
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    $scope.detailConfig = { erDetail: true, timeLine: true, eoDetail: true };
 
 }
