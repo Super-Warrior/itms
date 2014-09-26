25de550bd667e7adb8a0202710924e3466f50475
慕陶 <jihui.xjh@alibaba-inc.com>
2014-09-26 14:56:07 +0800

    feat(auth): add current logined user to httpheader

--- src/app/app.js
+++ src/app/app.js
@@ -1,47 +1,64 @@
 angular
-    .module('itms', [
-        'ui.router',
-        'ui.bootstrap',
-        'ngCookies',
-        'angular-loading-bar',
-        'common',
-        'itms.auth',
-        'itms.transport',
-        'itms.planning',
-        'itms.requirement',
-        'itms.common',
-        'itms.dashboard',
-        'itms.login'
-    ])
-    .run(['$rootScope', '$state', '$stateParams', '$log', '$location','auth', bootstrap])
-    .config(['$urlRouterProvider', '$stateProvider', routerConfig]);
+  .module('itms', [
+    'ui.router',
+    'ui.bootstrap',
+    'ngCookies',
+    'angular-loading-bar',
+    'common',
+    'itms.auth',
+    'itms.transport',
+    'itms.planning',
+    'itms.requirement',
+    'itms.common',
+    'itms.dashboard',
+    'itms.login'
+  ])
+  .run(['$rootScope', '$state', '$stateParams', '$log', '$location', 'auth', bootstrap])
+  .config(['$urlRouterProvider', '$stateProvider', routerConfig])
+  .config(['$httpProvider', httpConfig]);
 
-function routerConfig($urlRouterProvider, $stateProvider) {
-//    $urlRouterProvider
-//
-//        //        .when('/c?id', '/transportation/eventmaintenance')
-//        //        .when('/user/:id', '/transportation/eventmaintenance')
-//        .otherwise('dashboard');
-    $stateProvider
-        .state('app', {
-            abstract: true,
-            template: '<div data-ui-view></div>'
-        })
-        .state('app.user', {
-            abstract: true,
-            templateUrl: 'app/layout/shell.html',
-            controller: 'shellCtrl'
-        });
+function httpConfig($httpProvider) {
+  $httpProvider.interceptors.push('authInterceptor');
 }
 
-function bootstrap($rootScope, $state, $stateParams, $log, $location,auth) {
-    $rootScope.$state = $state;
-    $rootScope.$stateParams = $stateParams;
-    $log.info('app start');
-    if (auth.isLoginRequired()) {
-        $state.go('app.login');
-    }
-    else if ($location.$$path === '') {
-        $state.go('app.user.dashboard');
-    }
+angular.module('itms')
+  .factory('authInterceptor', ['$rootScope',function($rootScope) {
+    return {
+      request: function(config) {
+        config.headers = config.headers || {};
+        if($rootScope.currentUser) {
+          config.headers.Authorization = $rootScope.currentUser;
+        }
+        return config;
+      }
+    };
+  }]);
+
+function routerConfig($urlRouterProvider, $stateProvider) {
+  //    $urlRouterProvider
+  //
+  //        //        .when('/c?id', '/transportation/eventmaintenance')
+  //        //        .when('/user/:id', '/transportation/eventmaintenance')
+  //        .otherwise('dashboard');
+  $stateProvider
+    .state('app', {
+      abstract: true,
+      template: '<div data-ui-view></div>'
+    })
+    .state('app.user', {
+      abstract: true,
+      templateUrl: 'app/layout/shell.html',
+      controller: 'shellCtrl'
+    });
+}
+
+function bootstrap($rootScope, $state, $stateParams, $log, $location, auth) {
+  $rootScope.$state = $state;
+  $rootScope.$stateParams = $stateParams;
+  $log.info('app start');
+  if (auth.isLoginRequired()) {
+    $state.go('app.login');
+  } else if ($location.$$path === '') {
+    $state.go('app.user.dashboard');
+  }
 }
